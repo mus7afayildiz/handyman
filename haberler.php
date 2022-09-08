a4c0acb6e404b977ff922c5211170c87151be332
mus7afayildiz <mus7afayildiz@yandex.com>
2022-09-08 09:06:22 +0200

    added haberler.php

---
+++ haberler.php
@@ -0,0 +1,44 @@
+<?php include 'header.php'; ?>
+<!--==============================content================================-->
+<section id="content">
+  <div class="wrapper">
+    <article class="col-1">
+
+      <!-- While Baslangic -->
+      <div class="indent-left">
+        <h3 class="prev-indent-bot">HABER BLOGU</h3>
+        <div class="wrapper prev-indent-bot">
+          <figure class="img-indent"><img src="images/page2-img1.jpg" alt=""></figure>
+          <div class="extra-wrap">
+
+            <h6 class="prev-indent-bot">Sed ut perspiciatis unde omnis iste natus </h6>
+            Error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
+          </div>
+        </div>
+        <p class="p2">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatemt enim ad minima veniam, quis nostrum exercitationem.</p>
+        <a class="button-2" href="#">Read More</a>
+      </div>
+      <hr>
+      <!-- While Bitis -->
+
+    </article>
+    <article class="col-2">
+      <h4 style="font-size:20px;" class="p1">En Cok Okunan Haberler</h4>
+      <ul class="list-1">
+
+        <li><a href="#">Lorem ipsum dolor sitmet consectetur</a></li>
+       
+      </ul>
+    </article>
+  </div>
+</section>
+<!--==============================aside================================-->
+<aside>
+
+
+  <div class="block"></div>
+</aside>
+</div>
+</div>
+<!--==============================footer=================================-->
+<?php include 'footer.php'; ?>
